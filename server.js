38286d86d0591cc363d64a30146e3f392faf315d
Brant Cass <94635676+brantcass@users.noreply.github.com>
2023-07-18 03:06:28 +0200

    node modules

--- server.js
+++ server.js
@@ -1,39 +1,65 @@
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const server = express();
-const port = 3000;
 
+
+
+const port = 3000;
 
 //Static files
 
-server.use(express.static('public'));
+server.use(express.static("public"));
+server.use(bodyParser.urlencoded({extended: true}));
 
-server.use('/css', express.static(__dirname + '/public/css'));
-server.use('/js', express.static(__dirname + '/public/js'));
-server.use('/img', express.static(__dirname + '/public/img'));
+server.use("/css", express.static(__dirname + "/public/css"));
+server.use("/js", express.static(__dirname + "/public/js"));
+server.use("/img", express.static(__dirname + "/public/img"));
 
-
-server.get('/', (req, res) => {
-
-   res.sendFile(__dirname + '/views/index.html');
-  
- });  
-
-server.get('/waterconditions', (req, res) => {
-  res.sendFile(__dirname + '/views/waterconditions.html');
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-server.get('/fishinmyarea', (req, res) => {
-  res.sendFile(__dirname + '/views/fishinmyarea.html');
+server.get("/waterconditions", (req, res) => {
+  res.sendFile(__dirname + "/views/waterconditions.html");
 });
 
-server.get('/castingtips', (req, res) => {
-  res.sendFile(__dirname + '/views/castingtips.html');
+server.get("/fishinmyarea", (req, res) => {
+  res.sendFile(__dirname + "/views/fishinmyarea.html");
 });
 
-server.get('/fishfound', (req, res) => {
-  res.sendFile(__dirname + '/views/fishfound.html');
+server.get("/castingtips", (req, res) => {
+  res.sendFile(__dirname + "/views/castingtips.html");
+});
+
+server.get("/fishfound", (req, res) => {
+  res.sendFile(__dirname + "/views/fishfound.html");
+});
+
+server.post("/submit", (req, res) => {
+  const waterColor = req.body["water-color"];
+
+  switch (waterColor) {
+    case "clear":
+      res.redirect("/clearwater.html");
+      break;
+
+    case "murky":
+      res.redirect("/murkywater.html");
+      break;
+
+    case "green":
+      res.redirect("/greenwater.html");
+      break;
+
+    case "brown":
+      res.redirect("/brownwater.html");
+      break;
+
+    default:
+      res.redirect('/');
+  }
 });
 
 server.listen(3000, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+  console.log(`Server running at http://localhost:${port}`);
+});
